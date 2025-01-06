9c6c94f7ffd5c4b270d2bde6215a48a0b1682389
Erlengut <45758789+Erlengut@users.noreply.github.com>
2025-01-06 20:40:26 +0100

    Update settings.py

--- erlengut/settings.py
+++ erlengut/settings.py
@@ -197,3 +197,23 @@ ENABLE_SHARES=False
 BILLS_USERMENU = True
 DEFAULT_FROM_EMAIL = "info@solawi-erlengut.ch"
 IMPORT_EXPORT_EXPORT_PERMISSION_CODE = 'view'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {'format': '[%(asctime)s] %(levelname)s %(message)s'}
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+        },
+    },
+} 
