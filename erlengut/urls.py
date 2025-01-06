750c87cdcb08103623ec556c3b1765ecbfad0375
Erlengut <45758789+Erlengut@users.noreply.github.com>
2025-01-06 20:23:42 +0100

    Update urls.py

--- erlengut/urls.py
+++ erlengut/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^', include('juntagrico.urls')),
     re_path(r'^',include('juntagrico_billing.urls')),
-    re_path(r'^$', juntagrico.views.home),
     re_path(r'^impersonate/', include('impersonate.urls')),
 ]
